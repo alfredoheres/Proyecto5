e3b701c7054e2cda0d8572a4b4bbcfa2e33e4e96
alfredoheres <luisalfredoHE@outlook.com>
2024-03-05 21:33:38 -0600

    primer commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Proyecto5
+
+Esta es una aplicacion donde se puede obtener el histograma sobre el kilometraje y una grafica de dispercion del precio con respecto al kilometraje en una base de datos de venta de vehiculos.
--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')
 
+st.header('Análisis exploratorio de datos')
+st.write('Por favor, seleccione el check box deseado para el analisis deseado')
+
 # crear una casilla de verificación
 build_histogram = st.checkbox('Construir un histograma')
 if build_histogram:  # si la casilla de verificación está seleccionada
@@ -15,7 +18,6 @@ if build_histogram:  # si la casilla de verificación está seleccionada
 # crear una casilla de verificación
 build_scatter = st.checkbox('Construir un gráfico de dispersión')
 if build_scatter:  # si la casilla de verificación está seleccionada
-    st.write(
-        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
